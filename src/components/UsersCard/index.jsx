e0ebac576a294f8800546c0424ed72161b053d63
mroenca40 <mproenca.marketing@gmail.com>
2022-06-09 23:52:18 -0300

    add - UsersCard

---
+++ src/components/UsersCard/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { BsPencil, BsFillTrashFill } from 'react-icons/bs'
+
+import styles from './UsersCard.module.css'
+import { Link } from 'react-router-dom'
+
+const UsersCard = ({ id, name, email, birthday, age, handleRemove }) => {
+  return (
+    <div className={styles.user_card}>
+      <h4>{name}</h4>
+      <p><span>Email: </span>{email}</p>
+      <p><span>Birthday: </span>{birthday}</p>
+      <p><span>Age: </span>{age}</p>
+      <div className={styles.user_cart_actions}>
+        <Link to="/">
+          <BsPencil /> Edit
+        </Link>
+        <button>
+          <BsFillTrashFill /> Delete
+        </button>
+      </div>
+    </div>
+  )
+}
+
+UsersCard.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  birthday: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired,
+  handleRemove: PropTypes.func.isRequired
+}
+
+export default UsersCard
--- src/pages/Users/index.jsx
+++ src/pages/Users/index.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import axios from 'axios'
 import Message from '../../components/layout/Message'
 import Container from '../../components/layout/Container'
 import LinkButton from '../../components/layout/LinkButton'
 import styles from './Users.module.css'
+import UsersCard from '../../components/UsersCard'
 
 const Users = () => {
   const userId = JSON.parse(localStorage.getItem('userId'))
+  const [users, setUsers] = React.useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/users')
+      .then(res => {
+        setUsers(res.data.users)
+      })
+      .catch(err => console.log(err))
+  }, [])
 
   return (
     <div className={styles.users_container}>
@@ -16,7 +26,17 @@ const Users = () => {
       </div>
       {userId && <Message type="success" msg="User created successfully" />}
       <Container className="start">
-        <p>Projetos...</p>
+        {users.length > 0 &&
+          users.map((user) => (
+            <UsersCard
+              key={user.id}
+              id={user.id}
+              name={user.name}
+              email={user.email}
+              birthday={user.birthday}
+              age={user.age}
+            />
+          ))}
       </Container>
     </div>
   )
